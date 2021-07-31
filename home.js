0490f69ca25e187f934c110066a390b7b54a1ba7
mishikok <80839992+mishikok@users.noreply.github.com>
2021-07-31 19:36:19 +0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,8 @@
+function calculateSupply(){
+    var talll=document.getElementById('talll').value;
+    
+    var moneny= document.getElementById('squear').value; 
+    var totalSupply=(moneny * 170) * (talll); 
+    var result= document.getElementById('dollar').value=totalSupply; 
+  }
+  
